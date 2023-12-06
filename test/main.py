481ddbe3c64064ddcd9f7155f6428271f26c1c69
gs.corado <gs.corado@samsung.com>
2023-12-06 11:44:49 -0400

    atualização - troca de pytest para unittest

--- test/main.py
+++ test/main.py
@@ -1,12 +1,11 @@
-import pytest
+import unittest
 from appium import webdriver
 from page_object.gallery_page import GalleryPage
 
 
-class TestMovie:
+class TestMovie(unittest.TestCase):
 
-    @pytest.fixture
-    def driver(self, request):
+    def setUp(self):
         desired_cap = {
             "platformName": "Android",
             "platformVersion": "12",
@@ -14,13 +13,18 @@ class TestMovie:
             "appActivity": "com.samsung.android.gallery.app.activity.GalleryActivity",
             "newCommandTimeout": 30
         }
-        driver = webdriver.Remote('http://localhost:4723/wd/hub', desired_cap)
-        request.addfinalizer(driver.quit)
-        return driver
+        self.driver = webdriver.Remote('http://localhost:4723/wd/hub', desired_cap)
 
-    def test_create_movie(self, driver):
+    def tearDown(self):
+        self.driver.quit()
+
+    def test_create_movie(self):
         expected_text = "Select items"
-        gallery_page = GalleryPage(driver)
+        gallery_page = GalleryPage(self.driver)
         gallery_page.open_gallery_and_click_on_albums()
-        assert gallery_page.create_a_movie() == expected_text, f"Expected text: '{expected_text}'" \
-                                                               f", Actual text: '{gallery_page.create_a_movie()}'"
+        actual_text = gallery_page.create_a_movie()
+        self.assertEqual(actual_text, expected_text, f"Expected text: '{expected_text}', Actual text: '{actual_text}'")
+
+
+if __name__ == "__main__":
+    unittest.main()
